00228b8d2319065da7293c75050a7bf1818ce152
Lukas Bachschwell <lukas@lbsfilm.at>
2020-11-22 23:28:50 +0100

    Add some very crazy testing logic
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/naoina/go-stringutil v0.1.0 // indirect
 	github.com/naoina/toml v0.1.1
+	github.com/stretchr/testify v1.6.1
 	gopkg.in/yaml.v2 v2.3.0
 )
--- store_test.go
+++ store_test.go
@@ -1,8 +1,12 @@
 package store
 
 import (
+	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 type Cat struct {
@@ -51,7 +55,13 @@ func TestSaveLoad(t *testing.T) {
 
 	settingsFile := "preferences.toml"
 
-	err := Save(settingsFile, &settings)
+	err := Load(settingsFile, &settings)
+	if err != nil {
+		t.Fatalf("failed to load preferences: %s\n", err)
+		return
+	}
+
+	err = Save(settingsFile, &settings)
 	if err != nil {
 		t.Fatalf("failed to save preferences: %s\n", err)
 		return
@@ -70,4 +80,88 @@ func TestSaveLoad(t *testing.T) {
 	if !equal(settings, newSettings) {
 		t.Fatalf("broken")
 	}
+}
+
+func TestSaveLoadInvalid(t *testing.T) {
+	settings := Settings{
+		Age: 42,
+		Cats: []Cat{
+			{"Rudolph", true},
+			{"Patrick", false},
+			{"Jeremy", true},
+		},
+		RandomString: "gophers are gonna conquer the world",
+	}
+
+	settingsFile := "preferences.roml"
+	assert.Panics(t, func() { Save(settingsFile, &settings) }, "Save should panic on unknown format")
+	assert.Panics(t, func() { Load(settingsFile, &settings) }, "Load should panic on unknown format")
+
+	settingsFile = "preferences"
+	assert.Panics(t, func() { Save(settingsFile, &settings) }, "Save should panic without .")
+
+}
+
+type RomlMarshaller struct {
+	mock.Mock
+}
+
+// MarshalFunc is any marshaler.
+func (m *RomlMarshaller) MarshalFunc(v interface{}) ([]byte, error) {
+	return []byte{}, fmt.Errorf("Failed on purpouse")
+
+}
+
+func (m *RomlMarshaller) UnmarshalFunc(data []byte, v interface{}) error {
+	return fmt.Errorf("Failed on purpouse")
+}
+
+func TestCustomBrokenMarshaller(t *testing.T) {
+	settings := Settings{
+		Age: 42,
+		Cats: []Cat{
+			{"Rudolph", true},
+			{"Patrick", false},
+			{"Jeremy", true},
+		},
+		RandomString: "gophers are gonna conquer the world",
+	}
+
+	settingsFile := "preferences.roml"
+
+	m := RomlMarshaller{}
+
+	Register("roml", m.MarshalFunc, m.UnmarshalFunc)
+
+	err := Save(settingsFile, &settings)
+	assert.Error(t, err)
+	err = nil
+
+	err = Load(settingsFile, &settings)
+	assert.Error(t, err)
+
+	f, err := os.Create("preferences.roml")
+	if err != nil {
+		t.FailNow()
+	}
+	_, err = f.WriteString("invalid")
+	if err != nil {
+		t.FailNow()
+	}
+	f.Close()
+
+	err = Load(settingsFile, &settings)
+	fmt.Println(err)
+	assert.Error(t, err)
+
+	os.Remove(settingsFile)
+
+	err = Load("/settingsFile.toml", &settings)
+	assert.Error(t, err)
+
+	err = Save("/settingsFile.toml", &settings)
+	assert.Error(t, err)
+
+	err = Save("/fuu/lqwjefnw/settingsFile.toml", &settings)
+	assert.Error(t, err)
 }
